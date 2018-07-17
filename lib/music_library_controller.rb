791c8a8cde0826d6df25452119725200f9b11842
valeria campilongo <cvaleriac@hotmail.it>
2018-07-17 00:38:02 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -7,33 +7,18 @@ class MusicLibraryController
   def call
     input = ""
     while input != "exit"
-      puts "Welcome to your music library!"
-      puts "To list all of your songs, enter 'list songs'."
-      puts "To list all of the artists in your library, enter 'list artists'."
-      puts "To list all of the genres in your library, enter 'list genres'."
-      puts "To list all of the songs by a particular artist, enter 'list artist'."
-      puts "To list all of the songs of a particular genre, enter 'list genre'."
-      puts "To play a song, enter 'play song'."
-      puts "To quit, type 'exit'."
-      puts "What would you like to do?"
-      input = gets.strip
+    puts "Welcome to your music library!"
+    puts "To list all of your songs, enter 'list songs'."
+    puts "To list all of the artists in your library, enter 'list artists'."
+    puts "To list all of the genres in your library, enter 'list genres'."
+    puts "To list all of the songs by a particular artist, enter 'list artist'."
+    puts "To list all of the songs of a particular genre, enter 'list genre'."
+    puts "To play a song, enter 'play song'."
+    puts "To quit, type 'exit'."
+    puts "What would you like to do?"
 
-      case input
-      when "list songs"
-        list_songs
-      when "list artists"
-        list_artists
-      when "list genres"
-        list_genres
-      when "list artist"
-        list_songs_by_artist
-      when "list genre"
-        list_songs_by_genre
-      when "play song"
-        play_song
+    input = gets.strip
       end
-
-    end
   end
 
   def list_songs
@@ -75,6 +60,7 @@ class MusicLibraryController
   end
 
   def play_song
+<<<<<<< HEAD
     puts "Which song number would you like to play?"
     input = gets.strip.to_i
     if (1..Song.all.length).include?(input)
@@ -83,17 +69,18 @@ class MusicLibraryController
 
     puts "Playing #{song.name} by #{song.artist.name}" if song
   end
-
+=======
     list_songs
     puts "Which song number would you like to play?"
     user_input = gets.strip.to_i
-
+ 
  if (1..Song.all.length).include?(user_input)
    song = Song.all.sort_by(&:name).each.with_index(1) do |song, index|[user_input]
  end
 
    puts "Playing #{song.name} by #{song.artist.name}" if song
 
-
-end
+ 
+end 
+>>>>>>> 7066de3775e06bd441a5d3d797dd593719a51dba
 end
