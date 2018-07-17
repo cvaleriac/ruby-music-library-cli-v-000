574dd17732edc9cd93d6ea77f216fecc82105ae3
valeria campilongo <cvaleriac@hotmail.it>
2018-07-17 00:39:29 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -18,6 +18,22 @@ class MusicLibraryController
     puts "What would you like to do?"
 
     input = gets.strip
+
+    case input
+      when "list songs"
+        list_songs
+      when "list artists"
+        list_artists
+      when "list genres"
+        list_genres
+      when "list artist"
+        list_songs_by_artist
+      when "list genre"
+        list_songs_by_genre
+      when "play song"
+        play_song
+      end
+      
       end
   end
 
@@ -73,14 +89,14 @@ class MusicLibraryController
     list_songs
     puts "Which song number would you like to play?"
     user_input = gets.strip.to_i
- 
+
  if (1..Song.all.length).include?(user_input)
    song = Song.all.sort_by(&:name).each.with_index(1) do |song, index|[user_input]
  end
 
    puts "Playing #{song.name} by #{song.artist.name}" if song
 
- 
-end 
+
+end
 >>>>>>> 7066de3775e06bd441a5d3d797dd593719a51dba
 end
